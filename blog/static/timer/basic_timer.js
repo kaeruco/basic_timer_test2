e4c2a006acbb6a05ecf363da20b4b1f2f346f11b
kaeruco <sumomiyo@gmail.com>
2021-01-19 14:13:40 +0900

    20210119 basic_timer test

--- blog/static/timer/basic_timer.js
+++ blog/static/timer/basic_timer.js
@@ -1,9 +1,29 @@
+var notification_count = 0;
+var sound_count = 0;
 var button_count = 0;
 
+var notification_sw = document.getElementById("notification_swith");
+var sound_sw = document.getElementById("sound_swith");
 var min_dis = document.getElementById("min_display");
 var sec_dis = document.getElementById("sec_display");
 var start_stop_btn = document.getElementById("start_stop_button");
 var reset_btn = document.getElementById("reset_button");
+
+// notification_swithの関数
+notification_sw.onchange = () => {
+  notification_count ++;
+  console.log('notification_count:', notification_count)
+
+  if (notification_count % 2 === 1) {
+    notification_fnc("通知 : ON", 3000);
+  }
+};
+
+// sound_switchの関数
+sound_sw.onchange = () => {
+  sound_count ++;
+  console.log('sound_count:', sound_count)
+};
 
 // start_stop_buttonの関数
 start_stop_btn.onclick = () => {
@@ -22,6 +42,7 @@ start_stop_btn.onclick = () => {
   if (button_count % 2 === 1) {
     console.log('button_count = Odd:', button_count);
 
+    console.log('start')
     start_stop_btn.textContent = "stop";
     reset_btn.disabled = true;
 
@@ -33,6 +54,7 @@ start_stop_btn.onclick = () => {
     }
 
     timer(count, base);
+
 
   } else {
     console.log('button_count = Even:', button_count)
@@ -67,12 +89,14 @@ let timer = async (count_time, base_time)=> {
   start_stop_btn.removeAttribute('disabled');
   reset_btn.removeAttribute('disabled');
 
-  end_time = performance.now();
-  console.log('total_time:', end_time - base_time);
+  finish_time = performance.now();
+  console.log('total_time:', finish_time - base_time);
 };
 
 // countdown関数
 let countdown = async (count_time, base_time) => {
+  let last_time = 0;
+
   for (var i = count_time; i >= 0; i--) {
     if (button_count % 2 === 0) {
       console.log('stop');
@@ -84,23 +108,49 @@ let countdown = async (count_time, base_time) => {
       }
 
       console.log('i > 0:', i);
-      now_time = performance.now();
+      let now_time= performance.now();
       let sleep_time = (1000 - (now_time - base_time) % 1000) || 1000;
-      console.log(sleep_time);　// 確認用。
 
       // sleep_timeのエラー対策
       if (sleep_time < 1) {
-        console.log("error. sleep_time sets '1000'.");
+        console.error('error. sleep_time :', sleep_time, 'changed to 1000');
         sleep_time = 1000;
       }
 
-      console.log(base_time); //  確認用。Chromeの他のタブを見ると、base_timeが変わってしまった。
       await sleep(sleep_time);
+
+      // consoleでの確認用
+      console.log('now_time :', now_time);
+      console.log('sleep_time :', sleep_time);
+
+      let interval_time = now_time - last_time;
+
+      if (i === count_time) {
+        ;
+      } else if (interval_time < 1025 && interval_time > 975) {
+        console.log('interval_time :', interval_time);
+      } else {
+        // interval_timeのエラー表示(現状、対策不可)
+        // choromeのタブの切り替えが原因か。
+        console.error('eroor. interval_time :', interval_time);
+      }
+
+      last_time = now_time;
+      // consoleでの確認用　ここまで
 
     } else {
       min_sec_display(i)
       console.log('i = 0:', i);
       display_abled();
+
+      if (notification_count % 2 === 1) {
+        notification_fnc("It's time.", 10 * 60 * 1000);
+      }
+
+      if (sound_count % 2 === 0) {
+        document.getElementById("sound").play();
+      }
+
     }
   }
 };
@@ -115,11 +165,19 @@ let min_sec_display = (time) => {
 };
 
 // displayへの入力を可能にする関数
-const display_abled = () =>{
+const display_abled = () => {
   button_count = 0;
   min_dis.removeAttribute('disabled');
   sec_dis.removeAttribute('disabled');
 };
 
-
-
+// 通知内容についての関数
+let notification_fnc = (comment, timeout_time)=> {
+  Push.create("Basic Timer", {
+    body : comment,
+    timeout : timeout_time,
+    onClick : function () {
+      this.close();
+    }
+  });
+};
